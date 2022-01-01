c34bd7c4251c495e9ac90869399d8da0c70b9c14
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-01-01 16:04:55 -0500

    Day 3: Part 1 (refactored)

--- day3/p1.py
+++ day3/p1.py
@@ -1,23 +1,17 @@
+from collections import defaultdict
 from sys import stdin
 
-lines = []
+memo = defaultdict(int)
 for line in stdin:
-    lines.append(line.strip())
-
-memo = [0] * len(lines[0])
-for line in lines:
-    for i, c in enumerate(line):
+    for i, c in enumerate(line.strip()):
         memo[i] += 1 if c == "1" else -1
-
-print(memo)
 
 gamma = ""
 epsilon = ""
 
-for value in memo:
+for value in memo.values():
     gamma += "1" if value > 0 else "0"
     epsilon += "0" if value > 0 else "1"
 
-print(gamma, epsilon, int(gamma, 2), int(epsilon, 2))
 ans = int(gamma, 2) * int(epsilon, 2)
 print(ans)
